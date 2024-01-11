e24e2685a56a4601db2dd22772d2faf342f3db8e
OHOKs <85952515+OHOKs@users.noreply.github.com>
2024-01-11 09:38:20 +0100

    Add files via upload

---
+++ asd/WpfApp1/Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Functions
+    {
+        public void WriteJSON(List<Item> items)
+        {
+
+        }
+        public List<Item> ReadJSON()
+        {
+            List<Item> items = new List<Item>();
+
+
+
+            return items;
+        }
+        public void UpdateItem(List<Item> items)
+        {
+
+        }
+        public void RemoveItem(List<Item> items)
+        {
+
+        }
+        public void AddItem(List<Item> items)
+        {
+
+        }
+        public void Display()
+        {
+
+        }
+    }
+}
---
+++ asd/WpfApp1/item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Item
+    {
+        public int[] date = new int[3];
+        public int sum { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+    }
+}
